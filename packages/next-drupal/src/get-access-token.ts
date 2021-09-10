8183a95486412a474344470ab974195d4858a932
arshad <arshad@chummun.com>
2021-09-10 12:40:49 +0400

    fix(next): update next

--- packages/next-drupal/src/get-access-token.ts
+++ packages/next-drupal/src/get-access-token.ts
@@ -5,19 +5,21 @@ export async function getAccessToken(): Promise<AccessToken> {
     return null
   }
 
-  const basic = Buffer.from(
-    `${process.env.DRUPAL_CLIENT_ID}:${process.env.DRUPAL_CLIENT_SECRET}`
-  ).toString("base64")
+  const username = Buffer.from(`${process.env.DRUPAL_CLIENT_ID}`).toString(
+    "base64"
+  )
+  const password = Buffer.from(`${process.env.DRUPAL_CLIENT_SECRET}`).toString(
+    "base64"
+  )
 
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_DRUPAL_BASE_URL}/oauth/token`,
     {
       method: "POST",
       headers: {
-        Authorization: `Basic ${basic}`,
         "Content-Type": "application/x-www-form-urlencoded",
       },
-      body: `grant_type=client_credentials`,
+      body: `grant_type=client_credentials&username=${username}&password=${password}`,
     }
   )
 
